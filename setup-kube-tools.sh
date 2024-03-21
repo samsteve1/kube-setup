6c8652dc40ea55be9cfe76ba76b043a3c6c4c3d0
Steve Idowu <idowusteve@ymail.com>
2024-03-21 00:47:49 -0400

    message

--- setup-kube-tools.sh
+++ setup-kube-tools.sh
@@ -8,7 +8,7 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --
 # This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt-get update
+sudo apt-get update sudo apt-get update --allow-insecure-repositories
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
